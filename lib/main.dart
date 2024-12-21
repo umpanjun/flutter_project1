a10f685e71650a3646402eabd30d15941b39698a
umpanjun <ranzkyle2102@gmail.com>
2024-12-21 15:52:20 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('My Flutter App')),
-        body: Center(child: Text('Hello, Flutter!')),
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor: Colors.pink, // สีชมพูสำหรับ AppBar
+        ),
+        body: Container(
+          color: Colors.purple[100], // พื้นหลังสีม่วงอ่อน
+          child: Center(
+            child: Text(
+              'Hello',
+              style: TextStyle(fontSize: 20), // เพิ่มข้อความ "Hello" ตรงกลาง
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Floating button pressed');
+          },
+          backgroundColor: Colors.pink[200], // สีของปุ่มลอย
+          child: Icon(Icons.phone), // ไอคอนโทรศัพท์
+        ),
       ),
     );
   }
